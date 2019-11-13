445d950bfa1a3a14687caf3a8ce971af3be66922
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2019-11-13 17:55:24 +0100

    session/calendar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,10 @@
 
 namespace App\Controller;
 
+use \DateInterval;
+use \DatePeriod;
+use \DateTime;
+
 class HomeController extends AbstractController
 {
 
@@ -19,8 +23,38 @@ class HomeController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function index()
+    public function index(int $month = 12)
     {
-        return $this->twig->render('Home/index.html.twig');
+        $startMonth = new DateTime('01-' . $month . '-2019');
+        $endMonth = new DateTime('24-' . $month . '-2019 23:59:59');
+
+        $days = new DatePeriod($startMonth, new DateInterval('P1D'), $endMonth);
+
+        $shuffleDays = [];
+        foreach ($days as $day) {
+            $shuffleDays[] = $day;
+        }
+        if (empty($_SESSION['openDays'])) {
+            shuffle($shuffleDays);
+            foreach ($shuffleDays as $shuffleDay) {
+                $_SESSION['openDays'][] = ['day' => $shuffleDay, 'status' => false];
+            }
+        }
+
+        return $this->twig->render('Home/index.html.twig', [
+            'days' => $_SESSION['openDays'],
+        ]);
+    }
+
+    public function open(string $pos)
+    {
+        $_SESSION['openDays'][$pos]['status'] = true;
+        header('Location: /home/index');
+    }
+
+    public function reset()
+    {
+        session_destroy();
+        header('Location: /home/index');
     }
 }
